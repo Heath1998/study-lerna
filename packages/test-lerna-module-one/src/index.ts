d857033d05bbf60a3ab333cd92f26529232d64a4
fanzehong <zehong.fan@shopee.com>
2024-04-26 18:46:41 +0800

    fix : eslint

--- packages/test-lerna-module-one/src/index.ts
+++ packages/test-lerna-module-one/src/index.ts
@@ -1,9 +1,9 @@
-import all from 'test-lerna-module-two';
+import all from "test-lerna-module-two";
 // const { sum }  = require('test-lerna-module-two');
 
-console.log(all.sum(1,2));
+console.log(all.sum(1, 2));
 
-const a = [1, 2, 3, 4, 5, 6, all.sum(1,6),8];
+const a = [1, 2, 3, 4, 5, 6, all.sum(1, 6), 8];
 
 const one = () => {
   const b = [...a, 2];
--- packages/test-lerna-module-two/src/index.ts
+++ packages/test-lerna-module-two/src/index.ts
@@ -1,7 +1,7 @@
- const sum = (a, b) => {
+const sum = (a, b) => {
   return a + b;
-}
+};
 
 export default {
-  sum
-}
+  sum,
+};
