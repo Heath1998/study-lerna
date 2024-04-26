523cc9a3d6c046a86af3aafafcbb164e86fd07a0
fanzehong <zehong.fan@shopee.com>
2024-04-26 17:06:18 +0800

    feat: add code

--- packages/test-lerna-module-one/src/index.ts
+++ packages/test-lerna-module-one/src/index.ts
@@ -3,7 +3,7 @@ import all from 'test-lerna-module-two';
 
 console.log(all.sum(1,2));
 
-const a = [1, 2, 3, 4, 5, 6];
+const a = [1, 2, 3, 4, 5, 6, all.sum(1,6)];
 
 const one = () => {
   const b = [...a, 2];
