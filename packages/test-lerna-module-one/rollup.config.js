3e14f6b932a877b96f4dcac1346e4ee301eafdd3
fanzehong <zehong.fan@shopee.com>
2024-05-06 17:33:23 +0800

    15

--- packages/test-lerna-module-one/rollup.config.js
+++ packages/test-lerna-module-one/rollup.config.js
@@ -1,36 +1,36 @@
 // rollup.config.js
-import resolve from 'rollup-plugin-node-resolve';
+import resolve from "rollup-plugin-node-resolve";
 
-import typescript from 'rollup-plugin-typescript2';
-import babel from '@rollup/plugin-babel';
+import typescript from "rollup-plugin-typescript2";
+import babel from "@rollup/plugin-babel";
 
 export default {
   // 入口
-  input: 'src/index.ts',
+  input: "src/index.ts",
   output: [
     {
       // 打包后文件
-      file: 'dist/index.js',
+      file: "dist/index.js",
       // 编译格式 commonjs umd amd
-      format: 'cjs',
-      name: '@anles/utils',
+      format: "cjs",
+      name: "@anles/utils",
     },
     {
       // 打包后文件
-      file: 'dist/index.mjs',
+      file: "dist/index.mjs",
       // 编译格式 commonjs umd amd
-      format: 'esm',
-      name: '@anles/utils',
-    }
+      format: "esm",
+      name: "@anles/utils",
+    },
   ],
   plugins: [
     resolve(),
     typescript(),
     babel({
       // exclude: 'node_modules/**',
-      babelHelpers: 'runtime',
-      extensions: ['.js', '.ts'],
+      babelHelpers: "runtime",
+      extensions: [".js", ".ts"],
     }),
-
   ],
+  external: ["test-lerna-module-two"],
 };
